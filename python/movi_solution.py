fa8ba5e330a1435cad366e88617e8f8a84cf2501
Luong NGUYEN <luongnv89@gmail.com>
2018-03-02 00:42:02 +0100

    Update

--- python/movi_solution.py
+++ python/movi_solution.py
@@ -19,22 +19,32 @@ import random
 # Global constants
 OUTPUT_POSTFIX = "_movi.out"
 OUTPUT_LOCATION = "output"
+MIN_DISTANCE = 150
 
 # Global variables
 bestCore = 0
 bestSolution = None
 list_rides = []
-
+list_vehicles = []
 # Main functions
 
 class Vehicle:
-    def __init__(self, id, x, y):
+    def __init__(self, id, x, y, steps):
         self.id = id
         self.x = x
         self.y = y
+        self.remain_steps = steps
+        self.rides = []
+        self.last_finish = 0
+
+    def takeRide(self, ride):
+        self.rides.append(ride)
+        self.last_finish = ride.l_finish
+        self.x = ride.end_x
+        self.y = ride.end_y
 
 class CityMap:
-    def init(self, rows, cols, nb_rides, nb_vehicle, bonus, steps):
+    def init(self, rows, cols, nb_rides, nb_vehicle, bonus, steps, file_path):
         self.rows = rows
         self.cols = cols
         self.nb_rides = nb_rides
@@ -42,6 +52,7 @@ class CityMap:
         self.bonus = bonus
         self.steps = steps
         self.data = [[0 for c in range(cols)] for r in range(rows)]
+        self.file_path = file_path
 
 class Ride:
     def __init__(self, r_id,  start_x, start_y, end_x, end_y, e_start, l_finish ):
@@ -52,6 +63,11 @@ class Ride:
         self.end_y = end_y
         self.e_start = e_start
         self.l_finish = l_finish
+        self.distance = getDistance(start_x, start_y, end_x, end_y)
+        self.timegap = l_finish - e_start
+
+def getDistance(start_x, start_y, end_x, end_y):
+    return abs(start_x - end_x) + abs(start_y - end_y)
 
 def printHelp(appName):
     """Print out the message if the user does not enter correct parameter
@@ -122,10 +138,14 @@ def readInput(file_path):
                 nb_rides = int(array[3])
                 bonus = int(array[4])
                 steps = int(array[5])
-                city_map.init(rows,cols,nb_rides,nb_vehicle,bonus,steps)
+                city_map.init(rows,cols,nb_rides,nb_vehicle,bonus,steps,file_path)
+                for v in range(nb_vehicle):
+                    new_vehicle = Vehicle(v, 0, 0, steps)
+                    list_vehicles.append(new_vehicle)
             else:
-                new_ride = parseRideData(line, line_index - 1)
-                list_rides.append(new_ride)
+                new_ride = parseRideData(line, line_index - 2)
+                if(new_ride.distance + getDistance(0,0,new_ride.start_x, new_ride.start_y) < steps and new_ride.distance > MIN_DISTANCE):
+                    list_rides.append(new_ride)
 
 def writeOutput(file_path, solution):
     """Write the output of a solution to the file
@@ -147,20 +167,34 @@ def evaluate(solution):
     """
     # TODO: to evaluate the solution
 
-def random_nb_ride(nb_remain_ride):
-    return random.randint(1, nb_remain_ride)
+def random_nb_ride(nb_remain_ride, nb_remain_vehicle):
+    if(nb_remain_ride == 1):
+        return 1
+    else:
+        return int(nb_remain_ride/nb_remain_vehicle)
+        # return random.randint(0,int(nb_remain_ride/nb_remain_vehicle + 10))
 
 def solveProblem():
     """Solve the problem
     """
     # TODO: Solve the problem
-    for v in range(city_map.nb_vehicle):
-        nb_ride = random_nb_ride(len(list_rides))
-        output = str(nb_ride)
-        for r in range(nb_ride):
-            output = output + ' ' + str(list_rides[r].ride_id)
-            list_rides.remove(list_rides[r])
-        writeToFile('movi_'+str(int(time.time())) + '.out',output + "\n")
+    
+    # Preprocess data
+    list_rides.sort(key=lambda x:x.e_start, reverse=True)
+    while len(list_rides) > 0:
+        for v in range(city_map.nb_vehicle):
+            if len(list_rides) > 0:
+                current_ride = list_rides.pop()
+                list_vehicles[v].takeRide(current_ride)
+            else:
+                break
+                
+    output_time = str(int(time.time()))
+    for veh in list_vehicles:
+        output = str(len(veh.rides))
+        for r in veh.rides:
+            output = output + ' ' + str(r.ride_id)
+        writeToFile(city_map.file_path+'_movi_'+ output_time + '.out',output + "\n")
 
 
 def showInput():
@@ -181,10 +215,18 @@ def start():
         return
     inputData = sys.argv[1]
     readInput(inputData)
+    # showInput()
+    print(city_map.rows)
+    print(city_map.cols)
+    print(city_map.nb_rides)
+    print(city_map.nb_vehicle)
+    # for v in list_rides:
+    #     print(json.dumps(v.__dict__))
 
-    showInput()
+    # for v in list_vehicles:
+    #     print(json.dumps(v.__dict__))
 
-    signal.signal(signal.SIGINT, signalHandler)
+    # signal.signal(signal.SIGINT, signalHandler)
     solveProblem()
 
 if __name__ == "__main__":
---
+++ python/test.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+python $1 a_example.in
+python $1 b_should_be_easy.in
+python $1 c_no_hurry.in
+python $1 d_metropolis.in
+python $1 e_high_bonus.in
